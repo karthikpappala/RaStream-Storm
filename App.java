d6ad0504ff3868af4443ee3d70f36f35bd277044
Karthik Pappala <159760384+karthikpappala@users.noreply.github.com>
2026-03-30 18:24:31 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,13 +1,1 @@
-package com.rastream;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
